4ed96c19b69ce0d88321aa8ee9eb946551313648
Lily <helloimlily@gmail.com>
2022-03-17 10:25:37 +0900

    0317

---
+++ taco-cloud/src/main/java/tacos/web/WebConfig.java
@@ -0,0 +1,14 @@
+package tacos.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	
+	public void addVieControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
+
+}
