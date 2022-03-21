85a490add3eb7296680351f6e710966108596afa
Lily <helloimlily@gmail.com>
2022-03-21 09:02:17 +0900

    0321

--- taco-cloud/src/main/java/tacos/Order.java
+++ taco-cloud/src/main/java/tacos/Order.java
@@ -1,21 +1,21 @@
 package tacos;
 
-import java.util.Date;
-
 import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-
+import javax.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
 import lombok.Data;
+import java.util.Date;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class Order {
-	
 	private Long id;
 	private Date placedAt;
-
+	
 	@NotBlank(message="Name is required")
 	private String deliveryName;
 	
@@ -34,11 +34,15 @@ public class Order {
 	@CreditCardNumber(message="Not a valid credit card number")
 	private String ccNumber;
 	
-	@Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$", 
+	@Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$",
 			message="Must be formatted MM/YY")
 	private String ccExpiration;
 	
-	@Digits(integer = 3, fraction = 0, message="Invalid CVV")
+	@Digits(integer=3, fraction=0, message="Invalid CVV")
 	private String ccCVV;
 	
+	private List<Taco> tacos = new ArrayList<>();
+	public void addDesign(Taco design) {
+		this.tacos.add(design);
+	}
 }
--- taco-cloud/src/main/java/tacos/web/DesignTacoController.java
+++ taco-cloud/src/main/java/tacos/web/DesignTacoController.java
@@ -3,45 +3,48 @@ package tacos.web;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import lombok.extern.slf4j.Slf4j;
+import tacos.Taco;
+import tacos.Order;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
-import tacos.Taco;
 import tacos.data.IngredientRepository;
+import javax.validation.Valid;
+import org.springframework.validation.Errors;
+import tacos.data.TacoRepository;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Slf4j
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
-	
 	private final IngredientRepository ingredientRepo;
 	
+	private TacoRepository tacoRepo;
+	
 	@Autowired
-	public DesignTacoController(IngredientRepository ingredientRepo) {
+	public DesignTacoController(IngredientRepository ingredientRepo, TacoRepository tacoRepo) {
 		this.ingredientRepo = ingredientRepo;
+		this.tacoRepo = tacoRepo;
 	}
-
+	
 	@GetMapping
 	public String showDesignForm(Model model) {
-		
 		List<Ingredient> ingredients = new ArrayList<>();
-		
 		ingredientRepo.findAll().forEach(i -> ingredients.add(i));
 		
 		Type[] types = Ingredient.Type.values();
 		for (Type type : types) {
-			model.addAttribute(type.toString().toLowerCase(), 
+			model.addAttribute(type.toString().toLowerCase(),
 					filterByType(ingredients, type));
 		}
 		
@@ -49,21 +52,33 @@ public class DesignTacoController {
 		
 		return "design";
 	}
-
-	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+	
+	private List<Ingredient> filterByType(
+			List<Ingredient> ingredients, Type type) {
 		return ingredients
 				.stream()
 				.filter(x -> x.getType().equals(type))
 				.collect(Collectors.toList());
 	}
 	
+	@ModelAttribute(name = "order")
+	public Order order() {
+		return new Order();
+	}
+	
+	@ModelAttribute(name = "taco")
+	public Taco taco() {
+		return new Taco();
+	}
+
 	@PostMapping
-	public String processDesign(@Valid Taco design, Errors errors) {
-		if(errors.hasErrors()) {
+	public String processDesign(@Valid Taco design, Errors errors, @ModelAttribute Order order) {
+		if (errors.hasErrors()) {
 			return "design";
 		}
 		
-		log.info("Processing design: " + design);
+		Taco saved = tacoRepo.save(design);
+		order.addDesign(saved);
 		
 		return "redirect:/orders/current";
 	}
